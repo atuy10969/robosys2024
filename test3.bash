f97a996e5518ce406dc88576c9ac568491bb86f1
Yuta Kannaka <s23c1041lj@s.chibakoudai.jp>
2024-11-26 03:28:06 +0900

    test3 change

--- test3.bash
+++ test3.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 ng () {
     echo "${1}行目が違うよ"
@@ -13,31 +13,48 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 # `average` のテスト
-if [ -x ./average ]; then
-    out=$(seq 5 | ./average)
-    [ "${out}" = "3.0" ] || ng "$LINENO"
+out=$(seq 5 | ./average)
+[ "${out}" = "3.0" ] || ng "$LINENO"  # 1+2+3+4+5 = 15; 平均は 15/5 = 3.0
 
-    out=$(echo あ | ./average)
-    [ "$?" = 1 ] || ng "$LINENO"
+out=$(echo あ | ./average)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-    out=$(echo | ./average)
-    [ "$?" = 1 ] || ng "$LINENO"
-else
-    echo "average プログラムが見つかりません。" >&2
-    res=1
-fi
+out=$(echo | ./average)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-# 結果を表示
-if [ "$res" = 0 ]; then
-    echo "すべてのテストが成功しました。"
-else
-    echo "いくつかのテストが失敗しました。"
-fi
+# 最大値のテスト
+out=$(seq 5 | ./max)
+[ "${out}" = 5 ] || ng "$LINENO"  # 1, 2, 3, 4, 5 の最大値は 5
 
+out=$(echo あ | ./max)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./max)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+# 最小値のテスト
+out=$(seq 5 | ./min)
+[ "${out}" = 1 ] || ng "$LINENO"  # 1, 2, 3, 4, 5 の最小値は 1
+
+out=$(echo あ | ./min)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./min)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
 
