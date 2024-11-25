a6c53bd37d2155fa80a0576ac052665cf3e18491
Yuta Kannaka <s23c1041lj@s.chibakoudai.jp>
2024-11-26 04:47:04 +0900

    test3 change ok

--- test3.bash
+++ test3.bash
@@ -7,58 +7,18 @@ ng () {
 
 res=0
 
-# `plus` のテスト
-out=$(echo -e "10\n20\n30" | ./plus)  # 複数行の入力
-[ "${out}" = "60" ] || ng "$LINENO"
+# 平均値のテスト
+out=$(echo "10 20 30" | ./average.txt)
+[ "${out}" = "20.0" ] || ng "$LINENO"
 
-out=$(echo "10 20 30" | ./plus)  # 1行で複数の数値（対応できるようにコード修正済み）
-[ "${out}" = "60" ] || ng "$LINENO"
+# 最小値のテスト
+out=$(echo "10 20 30" | ./min.txt)
+[ "${out}" = "10.0" ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"  # 数字以外はエラー
-[ "${out}" = "" ] || ng "$LINENO"
+# 最大値のテスト
+out=$(echo "10 20 30" | ./max.txt)
+[ "${out}" = "30.0" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "$LINENO"  # 空入力はエラー
-[ "${out}" = "" ] || ng "$LINENO"
-
-# `average` のテスト
-out=$(seq 5 | ./average.txt)  # 1, 2, 3, 4, 5 の平均値 = 3.0
-[ "${out}" = "3.0" ] || ng "$LINENO"
-
-out=$(echo あ | ./average.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./average.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-# `min` のテスト
-out=$(seq 5 | ./min.txt)  # 最小値は 1
-[ "${out}" = "1" ] || ng "$LINENO"
-
-out=$(echo あ | ./min.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./min.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-# `max` のテスト
-out=$(seq 5 | ./max.txt)  # 最大値は 5
-[ "${out}" = "5" ] || ng "$LINENO"
-
-out=$(echo あ | ./max.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./max.txt)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-# 結果判定
 [ "$res" = 0 ] && echo OK
 exit $res
 
