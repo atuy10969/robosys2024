efec9bd25cc4b9ffc48ec03541912dcd07b06dcf
Yuta Kannaka <s23c1041lj@s.chibakoudai.jp>
2024-11-26 20:29:59 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+i#!/bin/bash 
 
 ng () {
     echo "${1}行目が違うよ"
@@ -31,5 +31,28 @@ out=$(echo | ./average.txt)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+# `max` のテスト
+out=$(seq 5 | ./max.txt)
+[ "${out}" = "5" ] || ng "$LINENO"  # 最大値は 5
+
+out=$(echo "3 1 4 1 5 9" | ./max.txt)
+[ "${out}" = "9" ] || ng "$LINENO"  # 最大値は 9
+
+out=$(echo | ./max.txt)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+# `min` のテスト
+out=$(seq 5 | ./min.txt)
+[ "${out}" = "1" ] || ng "$LINENO"  # 最小値は 1
+
+out=$(echo "3 1 4 1 5 9" | ./min.txt)
+[ "${out}" = "1" ] || ng "$LINENO"  # 最小値は 1
+
+out=$(echo | ./min.txt)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
+
